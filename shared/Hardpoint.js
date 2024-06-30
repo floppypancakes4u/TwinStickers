cadf3734e6cf74ca19c7e9fc881ce096618b1d0b
floppypancakes4u <floppypancakes4u@gmail.com>
2024-06-30 09:50:24 -0700

    Broken conversion..

--- shared/Hardpoint.js
+++ shared/Hardpoint.js
@@ -1,1 +1,30 @@
-"use strict";
+// src/hardpoint.ts
+import { HardpointDataTable } from 'HardpointDataTable';
+export class TSHardPoint {
+    constructor({ id, parentActor, x, y, classData = HardpointDataTable["devBlaster"] }) {
+        this.id = id;
+        this.parentActor = parentActor;
+        this.targetActor = null;
+        this.worldX = 0;
+        this.worldY = 0;
+        this.offsetX = x;
+        this.offsetY = y;
+        this.classData = classData;
+        this.damageSpawnPoints = this.classData.damageSpawnerOffsets.length;
+        this.currentDamageSpawnerIndex = 0;
+        this.rotationSpeed = classData.rotationSpeed; // Speed at which the hardpoint rotates towards its target, in radians per frame
+        this.damagePerHit = 1;
+        this.rateOfFire = 10; // 10 times a second.
+        this.timeSinceLastShot = 0; // Time tracker for rate of fire
+        this.distance = 1000; // max range of the hardpoint
+        this.baseRotation = 0; // default rotation to face right
+        this.localRotation = this.baseRotation; // default rotation to face right
+        this.firingAngle = 360; // angle of the firing arc
+        this.drawFiringAngles = false;
+        this.active = false;
+    }
+    // Example of a void method
+    activate() {
+        this.active = true;
+    }
+}
--- static/classes/Hardpoint.js
+++ static/classes/Hardpoint.js
@@ -1,33 +1,11 @@
 import { distance, log } from "../shared/helpers.js";
 import { MathHelper } from "../shared/mathhelper.js";
 import { HardpointDataTable } from "../shared/HardpointDataTable.js";
-import { TSHardPoint } from "../../src/Hardpoint.ts"
+import { TSHardPoint } from "../shared/Hardpoint.js"
 
 export class Hardpoint extends TSHardPoint {
     constructor({ id, parentActor, x, y, classData = HardpointDataTable["devBlaster"] }) {
-        this.id = id;
-        this.parentActor = parentActor;
-        this.targetActor = null;
-        this.worldX = 0;
-        this.worldY = 0;
-        this.offsetX = x;
-        this.offsetY = y;
-        this.classData = classData;
-        this.damageSpawnPoints = this.classData.damageSpawnerOffsets.length;
-        this.currentDamageSpawnerIndex = 0;
-        this.rotationSpeed = classData.rotationSpeed; // Speed at which the hardpoint rotates towards its target, in radians per frame
-    
-        this.damagePerHit = 1;
-        this.rateOfFire = 10; // 10 times a second.
-        this.timeSinceLastShot = 0; // Time tracker for rate of fire
-    
-        this.distance = 1000;  // max range of the hardpoint
-        this.baseRotation = 0;     // default rotation to face right
-        this.localRotation = this.baseRotation;     // default rotation to face right
-        this.firingAngle = 360;       // angle of the firing arc
-        this.drawFiringAngles = false;
-    
-        this.active = false;
+        super({id, parentActor, x, y, classData})
     }
 }
 
